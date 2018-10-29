4d43a18d6add6c4b4c4f24a731a987f7471cfe2d
Xiao Ling <lingxiao1002@gmail.com>
2018-10-29 15:20:49 +0800

    Added react-native-camera

--- Example/App.js
+++ Example/App.js
@@ -6,44 +6,89 @@
  * @flow
  */
 
-import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import React, { Component } from 'react';
+import { Platform, StyleSheet, Text, View, Dimensions } from 'react-native';
+
+import { RNCamera } from 'react-native-camera';
 
 const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
+	ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
+	android: 'Double tap R on your keyboard to reload,\n' + 'Shake or press menu button for dev menu'
 });
 
-type Props = {};
-export default class App extends Component<Props> {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
-      </View>
-    );
-  }
+export default class App extends Component {
+	constructor() {
+		super();
+		this.state = {
+			myText: 'My Original Text'
+		};
+	}
+
+	render() {
+		return (
+			<RNCamera
+				ref={(ref) => {
+					this.camera = ref;
+				}}
+				style={styles.preview}
+				type={RNCamera.Constants.Type.back}
+				flashMode={RNCamera.Constants.FlashMode.on}
+				permissionDialogTitle={'Permission to use camera'}
+				permissionDialogMessage={'We need your permission to use your camera phone'}
+				onGoogleVisionBarcodesDetected={({ barcodes }) => {
+
+				  if (barcodes) {
+				    this.setState({myText: JSON.stringify(barcodes)})
+				  }
+				  else {
+				    this.setState({myText: 'No barcode detected.'})
+				  }
+
+				}}
+				// onBarCodeRead={({ barcodes }) => {
+				// 	if (barcodes) {
+				// 		this.setState({ myText: JSON.stringify(barcodes) });
+				// 	} else {
+				// 		this.setState({ myText: 'No barcode detected.' });
+				// 	}
+				// }}
+			>
+				<View style={styles.textBg}>
+					<Text style={styles.scanText}>{this.state.myText}</Text>
+				</View>
+			</RNCamera>
+		);
+	}
+
+	takePicture = async function() {
+		console.log('taking a picture');
+		if (this.camera) {
+			const options = { quality: 0.5, base64: true };
+			const data = await this.camera.takePictureAsync(options);
+			console.log(data.uri);
+		}
+	};
 }
 
+const deviceHeight = Dimensions.get('window').height;
+
+const deviceWidth = Dimensions.get('window').width;
+
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+	textBg: {
+		width: deviceWidth,
+		height: 50,
+		justifyContent: 'center',
+		backgroundColor: '#43474a',
+		marginTop: 0
+	},
+	preview: {
+		flex: 1,
+		alignItems: 'center',
+		justifyContent: 'space-between'
+	},
+	scanText: {
+		color: 'white',
+		textAlign: 'center'
+	}
 });
